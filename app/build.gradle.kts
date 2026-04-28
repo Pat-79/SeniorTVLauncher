952f9935bc9acf7dd79bf2d4f9b3ef75229331a8
patrickquaedackers <Patrick.Quaedackers@dynalinq.eu>
2026-04-28 20:55:28 +0200

    Add activity-ktx and fragment-ktx dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,8 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.leanback:leanback:1.0.0")
